b5f273bc7afde8c9e1e30f444a17c71f8b55ea3a
Anandrajoriya <anandrajoriya03@gmail.com>
2024-08-14 00:33:08 +0530

    play ground setup for react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PlaygroundMain from './playground/playgroundmain';
+import Home from './playground/routes/home';
+
+function App() {
+  return (
+    <div className="App">
+      <PlaygroundMain/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TodoProject/todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../components/Button/button'
+
+const Todo = () => {
+  return (
+    <div>
+      <h1>this is my todo . Her is my todo</h1>
+      <Button  variant={'alert'}>Please click here</Button>
+    </div>
+  )
+}
+
+export default Todo
