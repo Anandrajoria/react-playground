3f183b1d46466a9eb510045cd450b0d66162b194
Anandrajoriya <anandrajoriya03@gmail.com>
2024-09-16 11:27:18 +0530

    togglecards

---
+++ src/playground/components/toggleCards/toggleCards.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {ToggleCard} from '../../../components'
+
+function ToggleCards() {
+  return (
+    <>
+    <ToggleCard/>
+    <ToggleCard 
+  heading='Tougher then before. smarter then ever'
+  imgsrc='kimetsu.jpg'
+  subTitle='i will win'
+  para='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit saepe dolores accusamus alias consectetur quaerat, quasia!'
+  btn1='office'
+  btn2='haker'
+    />
+    </>
+  )
+}
+
+export default ToggleCards
