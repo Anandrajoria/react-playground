cd1331e10caa71713a73113944fbb59ab8c55d40
Anandrajoriya <anandrajoriya03@gmail.com>
2024-10-06 15:41:57 +0530

    circles

---
+++ src/playground/components/circles/circles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Circle}  from "../../../components/circle";
+
+const Circles=()=>{
+    return(
+        <>
+            <Circle/>
+            
+        </>
+    )
+}
+
+export default Circles
