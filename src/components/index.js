b6b3ccb802ec35ee4b260924e0ace49d4adacdbe
Anandrajoriya <anandrajoriya03@gmail.com>
2024-10-06 23:45:56 +0530

    merge

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,13 @@
 export * from './Button'
 export *  from './Card'
 export * from './blogCard'
-<<<<<<< HEAD
-export * from './simplePage'
-export * from './testimonialPage'
-=======
-<<<<<<< Updated upstream
-export * from './simplePage'
-=======
 export * from './simplePage'
 export * from './testimonialPage'
 export * from './circle'
 export * from './toggleCard'
-export * from './lightBox'
+// export * from './lightBox'
 export * from './profileCard'
-export * from './gallery'
+// export * from './gallery'
 export * from './carousel'
 export * from './list'
 export * from './footer'
->>>>>>> Stashed changes
->>>>>>> Blogcard
